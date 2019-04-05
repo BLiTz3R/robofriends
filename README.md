8d4275adfe7174614ae0bfd32717147386b2b86e
George <bl1723r@gmail.com>
2019-04-06 00:18:51 +0300

    Updates

--- README.md
+++
@@ -1,5 +0,0 @@
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
-
-My first React app. A simple app, fetching a list of users, generating a robot image for each one and displaying them as a list of cards. With a searchbox too!
-
-Tutorial by udemy.com.
---
+++ precache-manifest.26e1a8c45ffe35e7495130f1b705e436.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "1446d6ec194445c3572d",
+    "url": "/robofriends/static/js/runtime~main.1446d6ec.js"
+  },
+  {
+    "revision": "d171b077cb0ad9e97722",
+    "url": "/robofriends/static/js/main.d171b077.chunk.js"
+  },
+  {
+    "revision": "60b94042c327a796aef5",
+    "url": "/robofriends/static/js/1.60b94042.chunk.js"
+  },
+  {
+    "revision": "d171b077cb0ad9e97722",
+    "url": "/robofriends/static/css/main.2eaee5a5.chunk.css"
+  },
+  {
+    "revision": "60b94042c327a796aef5",
+    "url": "/robofriends/static/css/1.4698fb42.chunk.css"
+  },
+  {
+    "revision": "d7da19db2a871fb728b4a17467930d60",
+    "url": "/robofriends/index.html"
+  }
+];
